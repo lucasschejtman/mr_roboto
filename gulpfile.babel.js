95b790e00b15f60abe472e3116bddf253ee15c77
unknown <lucas.schejtman@gmail>
2015-06-18 23:00:26 +1200

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,145 @@
+import cp from 'child_process';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import del from 'del';
+import path from 'path';
+import runSequence from 'run-sequence';
+import webpack from 'webpack';
+import minimist from 'minimist';
+
+const $ = gulpLoadPlugins();
+const argv = minimist(process.argv.slice(2));
+const src = Object.create(null);
+
+let watch = false;
+let browserSync;
+
+// The default task
+gulp.task('default', ['sync']);
+
+gulp.task('clean', () => del(['.tmp', 'build/*', '!build/.git'], {dot: true}));
+
+gulp.task('assets', () => {
+  src.assets = 'src/public/**';
+  return gulp.src(src.assets)
+    .pipe($.changed('build/public'))
+    .pipe(gulp.dest('build/public'))
+    .pipe($.size({title: 'assets'}));
+});
+
+gulp.task('resources', () => {
+  src.resources = [
+    'package.json',
+    'src/content*/**',
+    'src/templates*/**'
+  ];
+  return gulp.src(src.resources)
+    .pipe($.changed('build'))
+    .pipe(gulp.dest('build'))
+    .pipe($.size({title: 'resources'}));
+});
+
+gulp.task('bundle', cb => {
+  let config = require('./webpack.config.js');
+  const bundler = webpack(config);
+  const verbose = !!argv.verbose;
+  let bundlerRunCount = 0;
+
+  function bundle(err, stats) {
+    if (err) {
+      throw new $.util.PluginError('webpack', err);
+    }
+
+    console.log(stats.toString({
+      colors: $.util.colors.supportsColor,
+      hash: verbose,
+      version: verbose,
+      timings: verbose,
+      chunks: verbose,
+      chunkModules: verbose,
+      cached: verbose,
+      cachedAssets: verbose
+    }));
+
+    if (++bundlerRunCount === (watch ? config.length : 1)) {
+      return cb();
+    }
+  }
+
+  if (watch) {
+    bundler.watch(200, bundle);
+  } else {
+    bundler.run(bundle);
+  }
+});
+
+gulp.task('build', ['clean'], cb => {
+  runSequence(['assets', 'resources'], ['bundle'], cb);
+});
+
+gulp.task('build:watch', cb => {
+  watch = true;
+  runSequence('build', () => {
+    gulp.watch(src.assets, ['assets']);
+    gulp.watch(src.resources, ['resources']);
+    cb();
+  });
+});
+
+gulp.task('serve', ['build:watch'], cb => {
+  src.server = [
+    'build/server.js',
+    'build/content/**/*',
+    'build/templates/**/*'
+  ];
+  let started = false;
+  let server = (function startup() {
+    var child = cp.fork('build/server.js', {
+      env: Object.assign({NODE_ENV: 'development'}, process.env)
+    });
+    child.once('message', message => {
+      if (message.match(/^online$/)) {
+        if (browserSync) {
+          browserSync.reload();
+        }
+        if (!started) {
+          started = true;
+          gulp.watch(src.server, function() {
+            $.util.log('Restarting development server.');
+            server.kill('SIGTERM');
+            server = startup();
+          });
+          cb();
+        }
+      }
+    });
+    return child;
+  })();
+
+  process.on('exit', () => server.kill('SIGTERM'));
+});
+
+gulp.task('sync', ['serve'], cb => {
+  browserSync = require('browser-sync');
+
+  browserSync({
+    logPrefix: 'BRT',
+    notify: false,
+    https: false,
+    proxy: 'localhost:5000'
+  }, cb);
+
+  process.on('exit', () => browserSync.exit());
+
+  gulp.watch(['build/**/*.*'].concat(
+    src.server.map(file => '!' + file)
+  ), file => {
+    browserSync.reload(path.relative(__dirname, file.path));
+  });
+});
+
+gulp.task('deploy', cb => {
+  const push = require('git-push');
+  const remote = 'https://github.com/lucasschejtman/mr_roboto';
+  push('./build', remote, cb);
+});
